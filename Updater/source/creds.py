33e089c51e0c468a6c15d5463b685613704bc85e
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-06-11 23:00:34 -0400

    Removed windows build data and added docs

--- Updater/source/creds.py
+++ Updater/source/creds.py
@@ -1,3 +1,18 @@
+"""
+This module provides functions to encrypt and decrypt the API key used to access the GitHub API.
+
+Functions:
+- generate_key(password: str) -> bytes: Generates a key from the given password.
+- encrypt(data: str, key: bytes) -> bytes: Encrypts the given data using the key.
+- decrypt(token: bytes, key: bytes) -> str: Decrypts the given token using the key.
+- dump_file() -> None: Dumps the encrypted API key to a file.
+- get_api_key() -> str: Returns the decrypted API key.
+
+Constants:
+- PASSWORD: The password used to generate the key.
+- API_KEY: The GitHub API key. Not included in plain text.
+"""
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
@@ -10,7 +25,16 @@ PASSWORD = r"HominumServer123!"
 API_KEY = r""
 
 
-def generate_key(password):
+def generate_key(password: str) -> bytes:
+    """
+    Generates a key from the given password.
+
+    Parameters:
+    - password (str): The password to generate the key from.
+
+    Returns:
+    - bytes: The generated key.
+    """
     password = password.encode()
     salt = password[::-1]
     kdf = PBKDF2HMAC(
@@ -24,19 +48,45 @@ def generate_key(password):
     return key
 
 
-def encrypt(data, key):
+def encrypt(data: str, key: bytes) -> bytes:
+    """
+    Encrypts the given data using the key.
+    
+    Parameters:
+    - data (str): The data to encrypt.
+    - key (bytes): The key to use for encryption.
+    
+    Returns:
+    - bytes: The encrypted data.
+    """
     data = data.encode()
     f = Fernet(key)
     return f.encrypt(data)
 
 
-def decrypt(token, key):
+def decrypt(token: bytes, key: bytes) -> str:
+    """
+    Decrypts the given token using the key.
+
+    Parameters:
+    - token (bytes): The token to decrypt.
+    - key (bytes): The key to use for decryption.
+
+    Returns:
+    - str: The decrypted data.
+    """
     f = Fernet(key)
     data = f.decrypt(token)
     return data.decode()
 
 
 def dump_file():
+    """
+    Dumps the encrypted API key to a file.
+
+    Returns:
+    - None
+    """
     key = generate_key(PASSWORD)
     encrypted_key = encrypt(API_KEY, key)
     with open("creds.pkl", "wb") as f:
@@ -44,6 +94,12 @@ def dump_file():
 
 
 def get_api_key():
+    """
+    Returns the decrypted API key.
+    
+    Returns:
+    - str: The decrypted API key.
+    """
     from source.path import APPLICATION_PATH
 
     with open(os.path.join(APPLICATION_PATH, "creds.pkl"), "rb") as f:
@@ -53,5 +109,5 @@ def get_api_key():
 
 
 if __name__ == "__main__":
-    #dump_file()
+    # dump_file()
     pass
