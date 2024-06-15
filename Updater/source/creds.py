fd2d1c9174b25c67322c22c88c07f1b28ad4307b
Eclik1 <118026928+Eclik1@users.noreply.github.com>
2024-06-15 02:56:54 -0400

    Secure API Token Locally (#2)
    * Removed creds.pkl and creation code
    
    * version update
    
    * update fmt
    
    * Updated Installer
    
    ---------
    
    Co-authored-by: Noah Blaszak <70231827+Trogiken@users.noreply.github.com>

--- Updater/source/creds.py
+++ Updater/source/creds.py
@@ -3,14 +3,12 @@ This module provides functions to encrypt and decrypt the API key used to access
 
 Functions:
 - generate_key(password: str) -> bytes: Generates a key from the given password.
-- encrypt(data: str, key: bytes) -> bytes: Encrypts the given data using the key.
 - decrypt(token: bytes, key: bytes) -> str: Decrypts the given token using the key.
-- dump_file() -> None: Dumps the encrypted API key to a file.
 - get_api_key() -> str: Returns the decrypted API key.
 
 Constants:
 - PASSWORD: The password used to generate the key.
-- API_KEY: The GitHub API key. Not included in plain text.
+- SALT: The salt used to generate the key.
 """
 
 from cryptography.fernet import Fernet
@@ -21,22 +19,19 @@ import base64
 import pickle
 import os
 
-PASSWORD = r"HominumServer123!"
-API_KEY = r""
+PASSWORD = r""
+SALT = r""
 
 
-def generate_key(password: str) -> bytes:
+def generate_key() -> bytes:
     """
     Generates a key from the given password.
-
-    Parameters:
-    - password (str): The password to generate the key from.
 
     Returns:
     - bytes: The generated key.
     """
-    password = password.encode()
-    salt = password[::-1]
+    password = PASSWORD.encode()
+    salt = SALT.encode()
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -45,23 +40,8 @@ def generate_key(password: str) -> bytes:
         backend=default_backend()
     )
     key = base64.urlsafe_b64encode(kdf.derive(password))
+
     return key
-
-
-def encrypt(data: str, key: bytes) -> bytes:
-    """
-    Encrypts the given data using the key.
-    
-    Parameters:
-    - data (str): The data to encrypt.
-    - key (bytes): The key to use for encryption.
-    
-    Returns:
-    - bytes: The encrypted data.
-    """
-    data = data.encode()
-    f = Fernet(key)
-    return f.encrypt(data)
 
 
 def decrypt(token: bytes, key: bytes) -> str:
@@ -77,20 +57,8 @@ def decrypt(token: bytes, key: bytes) -> str:
     """
     f = Fernet(key)
     data = f.decrypt(token)
+
     return data.decode()
-
-
-def dump_file():
-    """
-    Dumps the encrypted API key to a file.
-
-    Returns:
-    - None
-    """
-    key = generate_key(PASSWORD)
-    encrypted_key = encrypt(API_KEY, key)
-    with open("creds.pkl", "wb") as f:
-        pickle.dump(encrypted_key.decode(), f)
 
 
 def get_api_key():
@@ -102,12 +70,9 @@ def get_api_key():
     """
     from source.path import APPLICATION_PATH
 
-    with open(os.path.join(APPLICATION_PATH, "creds.pkl"), "rb") as f:
-        encrypted_key = pickle.load(f)
-    key = generate_key(PASSWORD)
-    return decrypt(encrypted_key, key)
+    with open(os.path.join(APPLICATION_PATH, "creds"), "rb") as f:
+        token = pickle.load(f)
+    key = generate_key()
+    decrypted_token = decrypt(token, key)
 
-
-if __name__ == "__main__":
-    # dump_file()
-    pass
+    return decrypted_token
