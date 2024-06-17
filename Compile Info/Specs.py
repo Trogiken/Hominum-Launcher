88f2df6a10ca39530f73fc136ba193f80572b17d
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-06-16 21:03:37 -0400

    Up build version

--- Compile Info/Specs.py
+++ Compile Info/Specs.py
@@ -2,7 +2,7 @@ import pyinstaller_versionfile
 
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
-    version="3.5.2.3",
+    version="3.5.2.4",
     company_name="Noah Blaszak",
     file_description="Hominum minecraft client",
     internal_name="Hominum Client",
