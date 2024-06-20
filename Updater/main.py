ba349061928803a4cab4ef9a560c1b9572196521
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-06-20 01:25:31 -0400

    Auth window managed

--- Updater/main.py
+++ Updater/main.py
@@ -18,7 +18,7 @@ from source import path
 from source import exceptions
 from source import gui
 
-PROGRAM_NAME = "Hominum Client"
+PROGRAM_NAME = "Hominum"
 VERSION = "4.5.3.4"
 SETTINGS = gui.get_settings()
 
@@ -26,10 +26,12 @@ SETTINGS = gui.get_settings()
 class AuthWindow(customtkinter.CTkToplevel):
     def __init__(self, master, email, **kwargs):
         super().__init__(master, **kwargs)
-        self.columnconfigure(0, weight=1)  # configure grid system
+        self.title("Authentication")
+        self.focus_set()
         self.email = email
         self.geometry("400x100")
         self.resizable(False, False)
+        self.columnconfigure(0, weight=1)  # configure grid system
 
         self.label = customtkinter.CTkLabel(self, text=f"Logging into {self.email}", font=SETTINGS.font_large)
         self.label.grid(row=0, column=0, pady=(20, 0))
@@ -38,8 +40,15 @@ class AuthWindow(customtkinter.CTkToplevel):
         self.progress.grid(row=1, column=0, pady=(20, 0))
         self.progress.start()
 
-        threading.Thread(target=self.auth, daemon=True).start()
-    
+        auth_thread = threading.Thread(target=self.auth, daemon=True)
+        auth_thread.start()
+
+        while auth_thread.is_alive():
+            self.update_idletasks()
+            self.update()
+
+        self.destroy()
+
     def auth(self):
         pmc = MCManager(email=self.email, context=(path.MAIN_DIR, path.WORK_DIR))
         try:
@@ -47,15 +56,17 @@ class AuthWindow(customtkinter.CTkToplevel):
             print(auth_session)
 
             if auth_session is None:
-                raise exceptions.AuthenticationFailed()
+                raise exceptions.AuthenticationFailed("Auth session is None")
         except Exception:
             print("Login failed")
-        
-        self.destroy()
 
-class LoginFrame(customtkinter.CTkFrame):
+
+class LoginWindow(customtkinter.CTkToplevel):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.title("Login")
+        self.geometry("450x200")
+        self.resizable(False, False)
         self.grid_columnconfigure(0, weight=1)  # configure grid system
 
         # email label
@@ -70,21 +81,29 @@ class LoginFrame(customtkinter.CTkFrame):
         self.button.grid(row=2, column=0, pady=(20, 0))
 
     def login(self):
+        self.button.configure(state="disabled")
         # create top level window
         self.auth_window = AuthWindow(master=self.master, email=self.entry.get())
-        self.auth_window.transient(self.master)
-
+        self.destroy()
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        self.title(PROGRAM_NAME)
         self.geometry("450x200")
         self.minsize(450, 200)
         self.grid_rowconfigure(0, weight=1)  # configure grid system
         self.grid_columnconfigure(0, weight=1)
 
-        self.my_frame = LoginFrame(master=self)
-        self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        # TODO: if not logged in show login window and wait for login
+        self.login_window = LoginWindow(master=self)
+        self.login_window.transient(self)
+        self.wait_window(self.login_window)
+
+        # TODO: if logged in show main window, else show error message and repeat login
+        self.focus_force()
+        self.main_window = customtkinter.CTkLabel(self, text="Logged in", font=SETTINGS.font_large)
+        self.main_window.grid(row=0, column=0)
 
 
 if __name__ == "__main__":
