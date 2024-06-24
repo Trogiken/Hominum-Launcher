a543803b3683b988f0ad78459001923d362bf627
Noah <70231827+Trogiken@users.noreply.github.com>
2024-06-24 19:02:42 -0400

    import fix

--- Updater/source/__init__.py
+++ Updater/source/__init__.py
@@ -1,7 +1,6 @@
 """Package for the Updater module."""
 
 from source import path
-from Updater.source.mc import remote
 from source import exceptions
 from source import creds
 from source import mc
