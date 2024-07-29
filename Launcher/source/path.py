9eb641620b3842c2ccfe747f372fc0526c7e9341
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-07-29 00:11:09 -0400

    Version Fix

--- Launcher/source/path.py
+++ Launcher/source/path.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 PROGRAM_NAME = "Hominum"
 PROGRAM_NAME_LONG = "Hominum Launcher"
-VERSION = "1.4.7.7"
+VERSION = "1.4.7.8"
 
 if getattr(sys, 'frozen', False):
     APPLICATION_DIR = pathlib.Path(sys.executable).parent
