80294fbb0883488f4aa9d1c99963e3a86bba09d0
Noah <70231827+Trogiken@users.noreply.github.com>
2024-07-30 23:38:22 -0400

    Init

--- Launcher/source/path.py
+++ Launcher/source/path.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 PROGRAM_NAME = "Hominum"
 PROGRAM_NAME_LONG = "Hominum Launcher"
-VERSION = "1.4.8.9"
+VERSION = "1.4.9.10"
 
 if getattr(sys, 'frozen', False):
     APPLICATION_DIR = pathlib.Path(sys.executable).parent
